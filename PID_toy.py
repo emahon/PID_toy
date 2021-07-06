f222a3de894fb06db6d086e5a8e8ab9a72439c0d
E Mahon <esmahon11@gmail.com>
2021-07-05 22:33:17 -0600

    Adding random perturbation, setpoint control, ability to skip entering a value, some initial work on GUI

--- PID_toy.py
+++ PID_toy.py
@@ -18,9 +18,26 @@
 
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+import random
 import time
 
 from matplotlib.widgets import Button
+
+# https://matplotlib.org/stable/gallery/user_interfaces/embedding_in_qt_sgskip.html
+#from matplotlib.backends.qt_compat import QtCore, QtWidgets
+#if QtCore.qVersion() >= "5.": # which it should be
+#    from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+#else:
+#    from matplotlib.backends.backend_qt4agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+#from matplotlib.figure import Figure
+#
+#from PyQt5.QtCore import pyqtRemoveInputHook
+#
+#class ApplicationWindow(QtWidgets.QMainWindow):
+#    def __init__(self):
+#        super().__init__()
+#        self._main = QtWidgets.QWidget()
+#        self.setCentralWidget(self._main)
 
 class pid_toy:
     def __init__(self):
@@ -70,11 +87,15 @@ class pid_toy:
         print("paused")
         self.paused = True
         ani.event_source.stop()
+        print("Hit enter to skip input and keep variable at current value")
         # get user input
         valid_p = False
         while (not valid_p):
             try:
                 p_string = input("Set value of p: ")
+                if (p_string == ""):
+                    valid_p = True
+                    break
                 self.p_constant = float(p_string)
                 valid_p = True
             except:
@@ -83,6 +104,9 @@ class pid_toy:
         while (not valid_i):
             try:
                 i_string = input("Set value of i: ")
+                if (i_string == ""):
+                    valid_i = True
+                    break
                 self.i_constant = float(i_string)
                 valid_i = True
             except:
@@ -91,10 +115,27 @@ class pid_toy:
         while (not valid_d):
             try:
                 d_string = input("Set value of d: ")
+                if (d_string == ""):
+                    valid_d = True
+                    break
                 self.d_constant = float(d_string)
                 valid_d = True
             except:
                 print("Invalid value for d")
+
+        valid_setpoint = False
+        while (not valid_setpoint):
+            try:
+                set_string = input("Setpoint value: ")
+                if (set_string == ""):
+                    valid_setpoint = True
+                    break
+                parsed_setpoint = float(set_string)
+                valid_setpoint = (parsed_setpoint >= 0) and (parsed_setpoint <= 100)
+                if (valid_setpoint):
+                    self.setpoint = parsed_setpoint
+            except:
+                print("Invalid setpoint")
 
         self.error = 0
         self.error_sum = 0
@@ -133,7 +174,8 @@ class pid_toy:
         self.friction_force = self.friction_coefficient*self.current_speed
 
         # todo add ability for disturbance function, replace 0 with it
-        self.acceleration = 0-self.friction_force-self.control_force
+        randDist = .1*(2*random.random() - 1)
+        self.acceleration = randDist-self.friction_force-self.control_force
 
         self.speedtext.set_text("Speed: " + self.format.format(self.current_speed))
 
@@ -167,36 +209,41 @@ class pid_toy:
 
         self.dtottext.set_text("Total D: " + self.format.format(self.d_constant*self.current_speed))
             
-        return line, self.curtext, self.totalforce, self.setpointtext, self.ptext, self.errtext, self.ptottext, self.itext, self.cumerrtext, self.itottext, self.dtext, self.speedtext, self.dtottext, axpause,
+        return line, setpointline, self.curtext, self.totalforce, self.setpointtext, self.ptext, self.errtext, self.ptottext, self.itext, self.cumerrtext, self.itottext, self.dtext, self.speedtext, self.dtottext, axpause,
 
-x_range = 150
-fig, ax = plt.subplots(figsize=(10,7)) # units are inches
-plt.subplots_adjust(bottom=0.3)
-axpause = plt.axes([0.81, 0.05, 0.1, 0.075])
 
-#axis to put text for values on
-axvalues = plt.axes([0.05, 0.05, 0.7, 0.125])
-#hide axes
-axvalues.set_xticks([])
-axvalues.set_yticks([])
+if (__name__ == '__main__'):
+    # https://stackoverflow.com/questions/40177743/why-does-input-cause-qcoreapplicationexec-the-event-loop-is-already-runnin
+    pyqtRemoveInputHook()    
 
-xs = list(range(0,x_range))
-ys = [0]*x_range
-line, = ax.plot(xs, ys, lw=2)
-setpointline, = ax.plot(xs, ys, lw=2)
+    x_range = 150
+    fig, ax = plt.subplots(figsize=(10,7)) # units are inches
+    plt.subplots_adjust(bottom=0.3)
+    axpause = plt.axes([0.81, 0.05, 0.1, 0.075])
 
-pid_toy_instance = pid_toy()
+    #axis to put text for values on
+    axvalues = plt.axes([0.05, 0.05, 0.7, 0.125])
+    #hide axes
+    axvalues.set_xticks([])
+    axvalues.set_yticks([])
 
-ax.set_ybound(0,100)
-ax.set_xlim(x_range,0)
-bpause = Button(axpause, "Pause")
-bpause.on_clicked(pid_toy_instance.pause)
+    xs = list(range(0,x_range))
+    ys = [0]*x_range
+    line, = ax.plot(xs, ys, lw=2)
+    setpointline, = ax.plot(xs, ys, lw=2)
 
-ani = animation.FuncAnimation(
-    fig,
-    pid_toy_instance.animate,
-    fargs=(line,setpointline),
-    interval=(1.0/120.0)*1000.0,
-    blit=True)
+    pid_toy_instance = pid_toy()
 
-plt.show()
+    ax.set_ybound(0,100)
+    ax.set_xlim(x_range,0)
+    bpause = Button(axpause, "Pause")
+    bpause.on_clicked(pid_toy_instance.pause)
+
+    ani = animation.FuncAnimation(
+        fig,
+        pid_toy_instance.animate,
+        fargs=(line,setpointline),
+        interval=(1.0/120.0)*1000.0,
+        blit=True)
+
+    plt.show()
--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 Toy program to play with PID loops
 
 Outputs using matplotlib
+
+Need to install the matplotlib and PyQt5 packages via pip to run
